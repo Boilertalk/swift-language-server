ad68f25a06acdca5b6f25b90938bec5fad2a8fe3
Koray Koska <koray@koska.at>
2018-04-03 23:06:25 +0200

    Add ServerCapabilities

--- Sources/SwiftLanguageServerLib/LanguageServer/ServerCapabilities.swift
+++ Sources/SwiftLanguageServerLib/LanguageServer/ServerCapabilities.swift
@@ -166,3 +166,154 @@ public struct StaticRegistrationOptions {
      */
     public let id: String?
 }
+
+public struct ServerCapabilities: Codable {
+
+    /**
+     * Defines how text documents are synced. Is either a detailed structure defining each notification or
+     * for backwards compatibility the TextDocumentSyncKind number.
+     *
+     * Our Server Library doesn't need backwards compatibility so we don't allow TextDocumentSyncKind here.
+     */
+    public let textDocumentSync: TextDocumentSyncOptions?
+
+    /**
+     * The server provides hover support.
+     */
+    public let hoverProvider: Bool?
+
+    /**
+     * The server provides completion support.
+     */
+    public let completionProvider: CompletionOptions?
+
+    /**
+     * The server provides signature help support.
+     */
+    public let signatureHelpProvider: SignatureHelpOptions?
+
+    /**
+     * The server provides goto definition support.
+     */
+    public let definitionProvider: Bool?
+
+    /**
+     * The server provides Goto Type Definition support.
+     *
+     * Since 3.6.0
+     */
+    public let typeDefinitionProvider: Bool?
+
+    /**
+     * The server provides Goto Implementation support.
+     *
+     * Since 3.6.0
+     */
+    public let implementationProvider: Bool?
+
+    /**
+     * The server provides find references support.
+     */
+    public let referencesProvider: Bool?
+
+    /**
+     * The server provides document highlight support.
+     */
+    public let documentHighlightProvider: Bool?
+
+    /**
+     * The server provides document symbol support.
+     */
+    public let documentSymbolProvider: Bool?
+
+    /**
+     * The server provides workspace symbol support.
+     */
+    public let workspaceSymbolProvider: Bool?
+
+    /**
+     * The server provides code actions.
+     */
+    public let codeActionProvider: Bool?
+
+    /**
+     * The server provides code lens.
+     */
+    public let codeLensProvider: CodeLensOptions?
+
+    /**
+     * The server provides document formatting.
+     */
+    public let documentFormattingProvider: Bool?
+
+    /**
+     * The server provides document range formatting.
+     */
+    public let documentRangeFormattingProvider: Bool?
+
+    /**
+     * The server provides document formatting on typing.
+     */
+    public let documentOnTypeFormattingProvider: DocumentOnTypeFormattingOptions?
+
+    /**
+     * The server provides rename support.
+     */
+    public let renameProvider: Bool?
+
+    /**
+     * The server provides document link support.
+     */
+    public let documentLinkProvider: DocumentLinkOptions?
+
+    /**
+     * The server provides color provider support.
+     *
+     * Since 3.6.0
+     */
+    public let colorProvider: ColorProviderOptions?
+
+    /**
+     * The server provides execute command support.
+     */
+    public let executeCommandProvider: ExecuteCommandOptions?
+
+    /**
+     * Workspace specific server capabilities
+     */
+    public let workspace: Workspace
+
+    public struct Workspace: Codable {
+
+        /**
+         * The server supports workspace folder.
+         *
+         * Since 3.6.0
+         */
+        public let workspaceFolders: WorkspaceFolders?
+
+        public struct WorkspaceFolders: Codable {
+
+            /**
+             * The server has support for workspace folders
+             */
+            public let supported: Bool?
+
+            /**
+             * Whether the server wants to receive workspace folder
+             * change notifications.
+             *
+             * If a strings is provided the string is treated as a ID
+             * under which the notification is registed on the client
+             * side. The ID can be used to unregister for these events
+             * using the `client/unregisterCapability` request.
+             *
+             * Note: String is not supported in this Library.
+             */
+            public let changeNotifications: Bool?
+        }
+    }
+
+    // How do we want to treat "Any" types with Codable?
+    // public let experimental: ???
+}
